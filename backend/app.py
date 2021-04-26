56e45fb5d969a5f0f6844d1469a60afe06609886
marcellof23 <01faria.marcello@gmail.com>
2021-04-27 00:30:54 +0700

    update terbaru:

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,21 @@
 from flask import Flask, request
+import pymongo
 from flask_cors import CORS
 import sqlite3
 import os
 import json
+from os import environ 
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 CORS(app)
+connection_url = 'mongodb+srv://dbUsers:bodoamatwoi@cluster0.wuqpo.mongodb.net/test'
+client = pymongo.MongoClient(connection_url)
+
+# Database
+Database = client.get_database('tasks')
+users  = Database.user
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, "tes.db")
 
@@ -26,6 +36,10 @@ def dir():
 def what_ismy_basedir():
     return basedir
 
+@app.route("/testing", methods=["GET"])
+def user_profile():
+    todos = users.find({}, {'_id': False})
+    return json.dumps([todo for todo in todos])
 
 @app.route("/api", methods=["GET"])
 def test_hello():
