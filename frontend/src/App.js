4585465bfe67d9d5009420abf726004573cd4bac
marcellof23 <01faria.marcello@gmail.com>
2021-04-27 18:10:53 +0700

    add template for chatting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,87 +1,44 @@
 import {useState,useEffect} from 'react'
+import BotSide from './components/BotSide'
+import ClientSide from "./components/ClientSide";
+import Messages from "./components/Messages";
+import Input from "./components/Input";
+import Header from "./components/Header";
+import "./style.css";
+import API from "./API";
 import {BrowserRouter as Router, Route} from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Tasks from './components/Tasks'
-import AddTask from './components/AddTask'
-import About from './components/About'
 const App = () => {
-  const [currentTime, setCurrentTime] = useState(0);
-  const [currentTask, setcurrentTask] = useState([]);
-  const [showAddTask, setShowAddTask] = useState(false)
-  const [tasks, setTasks] = useState([
-    {
-        id:1,
-        text: 'Doctors Appointment',
-        day: 'Feb 5th at 2:30pm',
-        reminder: true,
-    },
-    {
-        id:2,
-        text: 'Meeting at School',
-        day: 'Feb 6th at 1:30pm',
-        reminder: true,
-    },
-    {
-        id:3,
-        text: 'Food Shopping',
-        day: 'Feb 5th at 2:30pm',
-        reminder: false,
-    },
-])
+  const [messages, setMessages] = useState([]);
 
-useEffect(() => {
-  fetch('/time').then(res => res.json()).then(data => {
-    setCurrentTime(data.time);
-  });
-  fetch('/view').then(res => res.json()).then(data => {
-    setcurrentTask(data);
-    console.log(data);
-  });
-}, []);
+  useEffect(() => {
+    async function loadWelcomeMessage() {
+      setMessages([
+        <BotSide
+          key="0"
+          fetchMessage={async () => await API.GetChatbotResponse("hi")}
+        />
+      ]);
+    }
+    loadWelcomeMessage();
+  }, []);
 
-// Add Task
-const addTask = (task) => {
-  console.log(task)
-  const id = Math.floor(Math.random()*10000) + 1
-  const newTask = {id, ...task}
-  setTasks([...tasks, newTask])
-}
-// Delete Task
-const deleteTask = (id) => {
-  console.log('delete', id)
-  setTasks(tasks.filter((task)=>task.id !== id))
-}
+  const send = async text => {
+    const newMessages = messages.concat(
+      <ClientSide key={messages.length + 1} text={text} />,
+      <BotSide
+        key={messages.length + 2}
+        fetchMessage={async () => await API.GetChatbotResponse(text)}
+      />
+    );
+    setMessages(newMessages);
+  };
 
-// Toggle Reminder 
-const toggleReminder = (id) => {
-  console.log(id)
-  setTasks(tasks.map((task)=>task.id === id ? {...task, reminder: !task.reminder} : task))
-}
   return (
-    <Router>
-    <div className="container">
-        <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}/>
-        <Route path='/' exact render={(props) => (
-          <>
-            {showAddTask && <AddTask onAdd={addTask}/>}
-            {tasks.length>0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks to Show'}
-          </>
-        ) }/>
-        <Route path='/about' component={About}/>
-        <p>The current time is {currentTime}.</p>
-        {/* <p>The current task is {currentTask}.</p> */}
-        {currentTask.map((item, i) => (
-            <tr key={i}>
-                <td>{item.user_id}</td>
-                <td>{item.text}</td>
-                <td>{item.day}</td>
-                <td>{item.reminder}</td>
-            </tr>
-        ))}
-        <Footer/>
+    <div className="chatbot">
+      <Header />
+      <Messages messages={messages} />
+      <Input onSend={send} />
     </div>
-    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,31 +1,5 @@
-import PropTypes from 'prop-types'
-import {useLocation} from 'react-router-dom'
-import Button from './Button'
+import React from "react";
 
-
-const Header = ({title, onAdd, showAdd}) => {
-    const location = useLocation()
-    // const onClick = () => {
-    //     console.log('click')
-    // }
-    return (
-        <header className='header'>
-            <h1>{title}</h1>
-            {location.pathname === '/' && (<Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'Close' : 'Add'} onClick={onAdd}/>)}
-        </header>
-    )
+export default function Header() {
+  return <div className="header">&nbsp;React Chatbot UI</div>;
 }
-
-Header.defaultProps = {
-    title : 'Task tracker',
-}
-
-Header.propTypes  = {
-    title: PropTypes.string.isRequired
-}
-
-// CSS in JS
-// const headingStyle = {
-//     color:'red', backgroundColor:'black'
-// }
-export default Header
